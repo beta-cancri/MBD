73f8c0c7f63563bda0fb60d3c839f7bc02f9a48a
Diego Chinchay <149448905+beta-cancri@users.noreply.github.com>
2024-09-25 23:05:48 -0500

    homepage

--- my-birthday-app/src/components/HomePage.js
+++ my-birthday-app/src/components/HomePage.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import './HomePage.css';
 
 const HomePage = () => {
   return (
-    <div className="home-page">
-      <h1>Welcome to the Home Page!</h1>
+    <div className="photo-collage">
+      <img src="https://i.imgur.com/SGJkGe6.png" alt="Photo 1" className="collage-photo" />
+      <img src="https://i.imgur.com/zI1pl8w.jpg" alt="Photo 2" className="collage-photo" />
+      <img src="https://i.imgur.com/MrqgIhE.png" alt="Photo 3" className="collage-photo" />
+      <img src="https://i.imgur.com/CGoIHOO.png" alt="Photo 4" className="collage-photo" />
+      <img src="https://i.imgur.com/JBn7UEx.png" alt="Photo 5" className="collage-photo" />
+      <img src="https://i.imgur.com/l9Bqnsj.png" alt="Photo 6" className="collage-photo" />
+      <img src="https://i.imgur.com/lRBpKg1.jpg" alt="Photo 7" className="collage-photo" />
+      <img src="https://i.imgur.com/ghMhL8y.png" alt="Photo 8" className="collage-photo" />
     </div>
   );
 };
